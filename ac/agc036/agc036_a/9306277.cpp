f87a8e151ed15d776ac01db6fb77f9e9cbdc330e
e1810 <v.iceele1810@gmail.com>
2020-01-04 03:56:58 +0900

    add agc036/agc036_a/9306277.cpp

---
+++ ac/agc036/agc036_a/9306277.cpp
@@ -0,0 +1,12 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+        ll s, m=1e9;
+        scanf("%lld", &s);
+        printf("0 0 %lld 1 %lld %lld
+", m, (m-s%m)*(s%m!=0), s/m+(s%m!=0));
+        return 0;
+}
