246ae84ae68683f9b6047820f074cd1e1c1bd725
e1810 <v.iceele1810@gmail.com>
2020-02-03 01:33:06 +0900

    add arc003/arc003_1/9877598.c

---
+++ ac/arc003/arc003_1/9877598.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main(void){
+        int n, ans=0;
+        scanf("%d", &n);
+        getchar();
+        for(int i=0; i<n; i++){
+                char c = getchar();
+                int tmp = 'F'-c-1;
+                if(tmp>=0) ans += tmp;
+        }
+        printf("%.15lf
+", ans/(double)n);
+        return 0;
+}
