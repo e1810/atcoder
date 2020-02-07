98c1b3c354be29be09221116e48616cabc106a94
e1810 <v.iceele1810@gmail.com>
2020-02-08 06:13:23 +0900

    add abc118/abc118_c/9945530.py

---
+++ ac/abc118/abc118_c/9945530.py
@@ -0,0 +1,11 @@
+
+def gcd(a, b):
+    if(a==0): return b
+    if a<b: a,b=b,a
+    while b: a,b=b,a%b
+    return a
+
+n, *a = map(int, open(0).read().split())
+g = 0
+for i in a: g = gcd(g, i)
+print(g)
