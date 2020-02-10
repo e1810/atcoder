53219c7bcf0eaf1d97c6061015ea541fa6767a6e
e1810 <v.iceele1810@gmail.com>
2020-02-11 07:46:43 +0900

    add abc127/abc127_d/10034527.cpp

---
+++ ac/abc127/abc127_d/10034527.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+using ll = long long;
+using P = std::pair<ll,ll>;
+
+bool cmp(P a, P b){
+        return a.second>b.second;
+}
+
+int main(){
+        ll n, m;
+        scanf("%lld %lld", &n, &m);
+        std::priority_queue<ll, std::vector<ll>, std::greater<ll>> que;
+        for(ll i=0; i<n; i++){
+                ll tmp;
+                scanf("%lld", &tmp);
+                que.push(tmp);
+        }
+
+        std::vector<P> bc(m);
+        for(ll i=0; i<m; i++) scanf("%lld %lld", &bc[i].first, &bc[i].second);
+        std::sort(bc.begin(), bc.end(), cmp);
+
+        ll ans = 0;
+        for(ll i=0; i<m; i++){
+                for(ll j=0; j<bc[i].first; j++){
+                        ll tmp = que.top(); que.pop();
+                        if(tmp>bc[i].second){
+                                ans += tmp;
+                                goto outer;
+                        }
+                        ans += bc[i].second;
+                        if(que.empty()) goto outer;
+                }
+        }
+
+        outer:
+        while(!que.empty()){
+                ans += que.top();
+                que.pop();
+        }
+        printf("%lld
+", ans);
+        return 0;
+}
