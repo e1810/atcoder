3923db3c468bf6e8d30f909567495a0de7f35b33
e1810 <v.iceele1810@gmail.com>
2020-02-16 21:08:49 +0900

    add abc155/abc155_b/10138477.py

---
+++ ac/abc155/abc155_b/10138477.py
@@ -0,0 +1,8 @@
+
+n, *a = map(int, open(0).read().split())
+flag = False
+for i in range(n):
+    if a[i]%2==0:
+        if a[i]%3 and a[i]%5: flag = True
+if flag==False: print("APPROVED")
+else: print("DENIED")
