e3ba6238db6ebdece1bccbaa6018a5b4f14c9a19
e1810 <v.iceele1810@gmail.com>
2020-02-16 21:13:41 +0900

    add abc155/abc155_c/10141830.py

---
+++ ac/abc155/abc155_c/10141830.py
@@ -0,0 +1,16 @@
+
+n = int(input())
+dic = {}
+
+for i in range(n):
+    tmp = input()
+    if tmp in dic: dic[tmp] += 1
+    else: dic[tmp] = 1
+
+dic = sorted(dic.items())
+dic = sorted(dic, key=lambda x:-x[1])
+
+mx = dic[0][1]
+for i in dic:
+    if i[1]==mx: print(i[0])
+    else: break
