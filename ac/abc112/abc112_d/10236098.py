8a12a212553a73789d3e8d05614b86eb7c15be3b
e1810 <v.iceele1810@gmail.com>
2020-02-21 08:13:18 +0900

    add abc112/abc112_d/10236098.py

---
+++ ac/abc112/abc112_d/10236098.py
@@ -0,0 +1,14 @@
+
+n, m = map(int, input().split())
+
+fact = []
+for i in range(1, int(m**0.5)):
+    if m%i==0:
+        fact.append(i)
+        fact.append(m//i)
+fact.sort()
+
+ans = 1
+for i in fact:
+    if n*i<=m: ans = i
+print(ans)
