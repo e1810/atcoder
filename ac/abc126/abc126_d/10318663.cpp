e4b21c958a2b6b7f2008018262c4c713f0b86ee7
e1810 <v.iceele1810@gmail.com>
2020-02-24 10:09:40 +0900

    add abc126/abc126_d/10318663.cpp

---
+++ ac/abc126/abc126_d/10318663.cpp
@@ -0,0 +1,39 @@
+
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using Edge = std::pair<ll,ll>;
+#define to first
+#define cost second
+
+int main(){
+        ll n;
+        scanf("%ld", &n);
+
+        std::vector<std::vector<Edge>> links(n, std::vector<Edge>(0));
+        for(ll i=0; i<n-1; i++){
+                ll a, b, c;
+                scanf("%ld %ld %ld", &a, &b, &c);
+                links[a-1].push_back(Edge(b-1, c));
+                links[b-1].push_back(Edge(a-1, c));
+        }
+
+        std::vector<ll> color(n), used(n, 0);
+        color[0] = 1;
+
+        std::queue<Edge> que;
+        que.push(Edge(0, 0));
+        while(!que.empty()){
+                ll from = que.front().first;
+                que.pop();
+                used[from] = 1;
+                for(Edge p: links[from]){
+                        if(used[p.to]) continue;
+                        que.push(p);
+                        color[p.to] = color[from] ^ p.cost%2;
+                }
+        }
+
+        for(ll i: color) printf("%ld
+", i);
+        return 0;
+}
