5990e268f09261a4df6bec52694596eed21776e4
e1810 <v.iceele1810@gmail.com>
2020-02-27 07:36:07 +0900

    add abc087/arc090_a/10366055.py

---
+++ ac/abc087/arc090_a/10366055.py
@@ -0,0 +1,13 @@
+
+n = int(input())
+a = [list(map(int, input().split())) for i in range(2)]
+
+ans = 0
+for i in range(n):
+        cnt = flag = 0
+        for j in range(n):
+                if not flag: cnt += a[flag][j]
+                if i==j: flag = 1
+                if flag: cnt += a[flag][j]
+        ans = max(ans, cnt)
+print(ans)
