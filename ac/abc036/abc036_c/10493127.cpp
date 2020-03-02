5b6e92dce85f768874d44ea76b99ae0f74b96ee3
e1810 <v.iceele1810@gmail.com>
2020-03-02 22:26:58 +0900

    add abc036/abc036_c/10493127.cpp

---
+++ ac/abc036/abc036_c/10493127.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using P = std::pair<int,int>;
+
+int main(){
+        int n;
+        scanf("%d", &n);
+        std::vector<int> a(n);
+        std::map<int, int> prs;
+        for(int i=0; i<n; i++){
+                scanf("%d", &a[i]);
+                prs[a[i]]++;
+        }
+
+        int i=0;
+        for(auto &p: prs) p.second = i++;
+
+        for(int i: a) printf("%d
+", prs[i]);
+        return 0;
+}
