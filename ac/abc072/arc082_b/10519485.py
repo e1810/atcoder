5c4b626180a3ed107887c66e23dce137ccfae29e
e1810 <v.iceele1810@gmail.com>
2020-03-04 12:36:22 +0900

    add abc072/arc082_b/10519485.py

---
+++ ac/abc072/arc082_b/10519485.py
@@ -0,0 +1,10 @@
+
+n, *p = map(int, open(0).read().split())
+
+ans = 0
+for i in range(n-1):
+        if p[i]==i+1:
+                p[i], p[i+1] = p[i+1], p[i]
+                ans += 1
+if p[n-1]==n: ans += 1
+print(ans)
