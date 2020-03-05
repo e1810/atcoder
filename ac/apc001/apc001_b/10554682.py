5553695610ce4689edeae4cd08e0e04b5f0d1aa3
e1810 <v.iceele1810@gmail.com>
2020-03-06 08:13:33 +0900

    add apc001/apc001_b/10554682.py

---
+++ ac/apc001/apc001_b/10554682.py
@@ -0,0 +1,11 @@
+
+n = int(input())
+a, b = [list(map(int, input().split())) for i in range(2)]
+
+ap = bp = 0
+for i in range(n):
+        if a[i]>b[i]: bp += a[i]-b[i]
+        else:
+                if (b[i]-a[i])%2: bp += 1
+                ap += (b[i]-a[i]+1)//2
+print("YNeos"[ap<bp::2])
