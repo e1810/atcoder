f34198a9054b7c9149302ee53babc8492a65aeb2
e1810 <v.iceele1810@gmail.com>
2020-03-08 21:03:07 +0900

    add hitachi2020/hitachi2020_b/10670726.py

---
+++ ac/hitachi2020/hitachi2020_b/10670726.py
@@ -0,0 +1,9 @@
+
+A, B, m = map(int, input().split())
+a, b = [list(map(int, input().split()))for i in range(2)]
+
+ans = min(a) + min(b)
+for i in range(m):
+        x, y, c = map(int, input().split())
+        ans = min(ans, a[x-1]+b[y-1]-c)
+print(ans)
