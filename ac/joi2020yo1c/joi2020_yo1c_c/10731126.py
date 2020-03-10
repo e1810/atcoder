36ed30c80332a6fae1c2894e3371e235088e102d
e1810 <v.iceele1810@gmail.com>
2020-03-10 16:56:40 +0900

    add joi2020yo1c/joi2020_yo1c_c/10731126.py

---
+++ ac/joi2020yo1c/joi2020_yo1c_c/10731126.py
@@ -0,0 +1,9 @@
+
+n, *a=map(int,open(0).read().split())
+ans = cnt = prev = 0
+for i in a:
+        if i>=prev: cnt+=1
+        else: cnt=1
+        ans = max(ans, cnt)
+        prev = i
+print(ans)
