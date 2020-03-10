65f4cb8e641b93879cf1c87709a4667aa417be78
e1810 <v.iceele1810@gmail.com>
2020-03-10 21:09:40 +0900

    ignoretestest

--- ac/tools/atcoder-sidemenu-master/src/main.js
+++
@@ -1,9 +0,0 @@
-import { SideMenu } from './libs/sidemenu';
-import { SideMenuElement } from './libs/element';
-
-/**
- * サイドメニューのシングルトン
- */
-const sidemenu = new SideMenu();
-
-export { sidemenu, SideMenuElement };
