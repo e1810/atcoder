6f3156f89ec20387b220cae17b78445c93dce372
e1810 <v.iceele1810@gmail.com>
2020-03-22 21:12:41 +0900

    add abc159/abc159_d/11095068.py

---
+++ ac/abc159/abc159_d/11095068.py
@@ -0,0 +1,8 @@
+
+n, *a = map(int, open(0).read().split())
+x = [0]*n
+for i in a: x[i-1] += 1
+c = sum([i*(i-1)//2 for i in x])
+
+z = [c - x[i]*(x[i]-1)//2 + (x[i]-1)*(x[i]-2)//2 for i in range(n)]
+for i in a: print(z[i-1])
