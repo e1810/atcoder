bed0eefb5f91f0ad37dbd5af4717fa52ba37b226
e1810 <v.iceele1810@gmail.com>
2020-03-23 22:28:57 +0900

    add sumitrust2019/sumitb2019_c/11161606.c

---
+++ ac/sumitrust2019/sumitb2019_c/11161606.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+int main(void){
+        int x;
+        scanf("%d", &x);
+        int flag = 0;
+        for(int i=0; i<=1000; i++){
+                if(100*i<=x && x<=105*i) flag = 1;
+        }
+        printf("%d
+", flag);
+        return 0;
+}
