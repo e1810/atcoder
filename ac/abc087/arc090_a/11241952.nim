6c72012abbd19a7b05291a317b240287bff2b6e8
e1810 <v.iceele1810@gmail.com>
2020-03-27 23:36:44 +0900

    add abc087/arc090_a/11241952.nim

---
+++ ac/abc087/arc090_a/11241952.nim
@@ -0,0 +1,12 @@
+
+import strutils, sequtils
+var
+  n, a, b = stdin.readline.split.map(parseInt)
+  ans = 0
+for i in 0..<n[0]:
+  var tmp = 0
+  for j in 0..<n[0]:
+    if j<=i: tmp += a[j]
+    if j>=i: tmp += b[j]
+  ans = max(ans, tmp)
+echo ans
