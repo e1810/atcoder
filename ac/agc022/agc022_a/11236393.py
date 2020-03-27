928e033c9a8c3c3c7f6da895db3d5a2b28beb2a1
e1810 <v.iceele1810@gmail.com>
2020-03-27 19:28:30 +0900

    add agc022/agc022_a/11236393.py

---
+++ ac/agc022/agc022_a/11236393.py
@@ -0,0 +1,23 @@
+
+s = input()
+
+if len(s)<26:
+        st = set(s)
+        for c in "abcdefghijklmnopqrstuvwxyz":
+                if not c in st:
+                        print(s+c)
+                        break
+        else: print(-1)
+else:
+        prev = ord('a') - 1
+        i = len(s)
+        for c in s[::-1]:
+                if ord(c)>prev: i -= 1
+                else: break
+                prev = ord(c)
+        if i==0: print(-1)
+        else:
+                for c in sorted(s[i-1:]):
+                        if ord(c)>ord(s[i-1]):
+                                print(s[:i-1] + c)
+                                break
