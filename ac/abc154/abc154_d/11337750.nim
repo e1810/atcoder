7f747b08ba2dfc9e69635ddc38829f2b9e81c462
e1810 <v.iceele1810@gmail.com>
2020-03-29 16:05:13 +0900

    add abc154/abc154_d/11337750.nim

---
+++ ac/abc154/abc154_d/11337750.nim
@@ -0,0 +1,15 @@
+
+import strutils, sequtils
+var
+  a, p = stdin.readline.split.map(parseFloat)
+  n, k: int
+(n, k) = a.mapIt(it.int)
+
+p = @[0.0] & p
+for i in 1..n:
+  p[i] *= (p[i]+1) / (2*p[i])
+  p[i] += p[i-1]
+
+var ans = 0.0
+for i in 0..n-k: ans = max(ans, p[i+k]-p[i])
+echo ans
