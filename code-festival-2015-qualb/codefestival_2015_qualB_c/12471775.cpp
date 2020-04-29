e123b7b62760401a28c3959996c26a4e90786d78
e1810 <v.iceele1810@gmail.com>
2020-04-29 11:02:58 +0900

    add code-festival-2015-qualb/codefestival_2015_qualB_c/12471775.cpp

---
+++ code-festival-2015-qualb/codefestival_2015_qualB_c/12471775.cpp
@@ -0,0 +1,37 @@
+
+#pragma target("avx")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define rep(i,b,e) for(ll i=b; i<e; i++)
+#define be(vec) vec.begin(),vec.end()
+#define print(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define scan(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	V a(n), b(m);
+	scan(a); scan(b);
+	std::sort(be(a), std::greater<ll>());
+	std::sort(be(b), std::greater<ll>());
+	
+	bool ok = (n>=m);
+	rep(i, 0, m){
+		if(!ok) break;
+		ok &= (a[i]>=b[i]);
+	}
+	if(ok) puts("YES");
+	else puts("NO");
+	return 0;
+}
