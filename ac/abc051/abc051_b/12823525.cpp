35d316394f92302b3a3057135023873283c17540
e1810 <v.iceele1810@gmail.com>
2020-05-04 21:06:31 +0900

    add ac/abc051/abc051_b/12823525.cpp

---
+++ ac/abc051/abc051_b/12823525.cpp
@@ -0,0 +1,34 @@
+#pragma target("avx")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define rep(i,b,e) for(ll i=b; i<e; i++)
+#define be(vec) vec.begin(),vec.end()
+#define print(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define scan(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+        int k, s;
+        scanf("%d %d", &k, &s);
+        int ans = 0;
+        rep(i, 0, k+1){
+                rep(j, 0, k+1){
+                        if(0<=s-i-j and s-i-j<=k){
+                                ans++;
+                        }
+                }
+        }
+        printf("%d
+", ans);
+        return 0;
+}
