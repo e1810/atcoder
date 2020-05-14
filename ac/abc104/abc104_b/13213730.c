f30927d7c53a57ea461ae3175193b41d01e734e5
e1810 <v.iceele1810@gmail.com>
2020-05-15 00:10:35 +0900

    add ac/abc104/abc104_b/13213730.c

---
+++ ac/abc104/abc104_b/13213730.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(void){
+        char s[10], c;
+        int l = 0, i, cnt = 0;
+        bool ok = true;
+        while((c=getchar())!='
+') s[l++] = c;
+        for(i=0; i<l; i++){
+                if('a'<=s[i] && s[i]<='z') cnt++;
+                else if(s[i]=='C'){
+                        if(i<2 || l-2<i) ok = false;
+                }
+                else if(s[i]=='A'){
+                        if(i!=0) ok = false;
+                }
+                else ok = false;
+        }
+        if(ok && cnt==l-2) puts("AC");
+        else puts("WA");
+        return 0;
+}
