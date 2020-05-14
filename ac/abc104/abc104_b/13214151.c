7fcb8abb7c83f6083dbf8ba8df870b265a463b8b
e1810 <v.iceele1810@gmail.com>
2020-05-15 00:28:41 +0900

    add ac/abc104/abc104_b/13214151.c

---
+++ ac/abc104/abc104_b/13214151.c
@@ -0,0 +1,22 @@
+#pragma target("avx2")
+#pragma optimize("O3")
+#pragma optimize("unroll-loops")
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(void){
+        char s[10];
+        int l = 0, i, cnt = 0;
+        bool ok = true;
+        while((s[l]=getchar())!='
+') l++;
+        for(i=0; i<l; i++){
+                if('a'<=s[i] && s[i]<='z') cnt++;
+                else if(s[i]=='C') ok &= (1<i && i<l-1);
+                else if(s[i]=='A') ok &= (i==0);
+                else ok = false;
+        }
+        if(ok && cnt==l-2) puts("AC");
+        else puts("WA");
+        return 0;
+}
