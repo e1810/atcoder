3bc37749e3828c5ea1f54bee140d1890a4c09b23
e1810 <v.iceele1810@gmail.com>
2020-05-20 23:52:05 +0900

    add ac/arc031/arc031_1/13436321.cpp

---
+++ ac/arc031/arc031_1/13436321.cpp
@@ -0,0 +1,34 @@
+#pragma target("avx")
+#pragma optimize("O3")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define REP(i,b,e) for(ll i=b; i<e; i++)
+#define ALL(vec) vec.begin(),vec.end()
+#define PRINT(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define SCAN(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n=0;
+	char s[101], c;
+	while((c=getchar())!='
+') s[n++] = c;
+	bool ok = true;
+	for(int l=0, r=n-1; l<r; l++, r--){
+		if(s[l]!=s[r]) ok = false;
+	}
+	if(ok) puts("YES");
+	else puts("NO");
+	return 0;
+}
