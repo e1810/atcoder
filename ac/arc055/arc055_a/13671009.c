4906663a6f63d710efd9e64e1d8de9a3b5f104e3
e1810 <v.iceele1810@gmail.com>
2020-05-29 00:05:40 +0900

    add ac/arc055/arc055_a/13671009.c

---
+++ ac/arc055/arc055_a/13671009.c
@@ -0,0 +1,15 @@
+#pragma target("avx2")
+#pragma optimize("O3")
+#include<stdio.h>
+
+int main(void){
+        short n = 0;
+        char c;
+        while((c=getchar())!='
+') n = n *10 + c - '0';
+        n--;
+        putchar('1');
+        while(n--) putchar('0');
+        putchar('7');
+        return 0;
+}
