c286c16ca167a9badd03345660633c1bbe8ea21c
e1810 <v.iceele1810@gmail.com>
2020-05-30 22:51:21 +0900

    add ac/nomura2020/nomura2020_c/13740871.cpp

---
+++ ac/nomura2020/nomura2020_c/13740871.cpp
@@ -0,0 +1,209 @@
+#pragma target("avx")
+#pragma optimize("O3")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define REP(i,b,e) for(ll i=b; i<e; i++)
+#define ALL(vec) vec.begin(),vec.end()
+#define PRINT(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define SCAN(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+using namespace std;
+template< typename Monoid, typename OperatorMonoid = Monoid >
+struct LazySegmentTree {
+  using F = function< Monoid(Monoid, Monoid) >;
+  using G = function< Monoid(Monoid, OperatorMonoid) >;
+  using H = function< OperatorMonoid(OperatorMonoid, OperatorMonoid) >;
+
+  int sz, height;
+  vector< Monoid > data;
+  vector< OperatorMonoid > lazy;
+  const F f;
+  const G g;
+  const H h;
+  const Monoid M1;
+  const OperatorMonoid OM0;
+
+
+  LazySegmentTree(int n, const F f, const G g, const H h,
+                  const Monoid &M1, const OperatorMonoid OM0)
+      : f(f), g(g), h(h), M1(M1), OM0(OM0) {
+    sz = 1;
+    height = 0;
+    while(sz < n) sz <<= 1, height++;
+    data.assign(2 * sz, M1);
+    lazy.assign(2 * sz, OM0);
+  }
+
+  void set(int k, const Monoid &x) {
+    data[k + sz] = x;
+  }
+
+  void build() {
+    for(int k = sz - 1; k > 0; k--) {
+      data[k] = f(data[2 * k + 0], data[2 * k + 1]);
+    }
+  }
+
+  inline void propagate(int k) {
+    if(lazy[k] != OM0) {
+      lazy[2 * k + 0] = h(lazy[2 * k + 0], lazy[k]);
+      lazy[2 * k + 1] = h(lazy[2 * k + 1], lazy[k]);
+      data[k] = reflect(k);
+      lazy[k] = OM0;
+    }
+  }
+
+  inline Monoid reflect(int k) {
+    return lazy[k] == OM0 ? data[k] : g(data[k], lazy[k]);
+  }
+
+  inline void recalc(int k) {
+    while(k >>= 1) data[k] = f(reflect(2 * k + 0), reflect(2 * k + 1));
+  }
+
+  inline void thrust(int k) {
+    for(int i = height; i > 0; i--) propagate(k >> i);
+  }
+
+  void update(int a, int b, const OperatorMonoid &x) {
+    thrust(a += sz);
+    thrust(b += sz - 1);
+    for(int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+      if(l & 1) lazy[l] = h(lazy[l], x), ++l;
+      if(r & 1) --r, lazy[r] = h(lazy[r], x);
+    }
+    recalc(a);
+    recalc(b);
+  }
+
+  Monoid query(int a, int b) {
+    thrust(a += sz);
+    thrust(b += sz - 1);
+    Monoid L = M1, R = M1;
+    for(int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+      if(l & 1) L = f(L, reflect(l++));
+      if(r & 1) R = f(reflect(--r), R);
+    }
+    return f(L, R);
+  }
+
+  Monoid operator[](const int &k) {
+    return query(k, k + 1);
+  }
+
+  template< typename C >
+  int find_subtree(int a, const C &check, Monoid &M, bool type) {
+    while(a < sz) {
+      propagate(a);
+      Monoid nxt = type ? f(reflect(2 * a + type), M) : f(M, reflect(2 * a + type));
+      if(check(nxt)) a = 2 * a + type;
+      else M = nxt, a = 2 * a + 1 - type;
+    }
+    return a - sz;
+  }
+
+  template< typename C >
+  int find_first(int a, const C &check) {
+    Monoid L = M1;
+    if(a <= 0) {
+      if(check(f(L, reflect(1)))) return find_subtree(1, check, L, false);
+      return -1;
+    }
+    thrust(a + sz);
+    int b = sz;
+    for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if(a & 1) {
+        Monoid nxt = f(L, reflect(a));
+        if(check(nxt)) return find_subtree(a, check, L, false);
+        L = nxt;
+        ++a;
+      }
+    }
+    return -1;
+  }
+
+
+  template< typename C >
+  int find_last(int b, const C &check) {
+    Monoid R = M1;
+    if(b >= sz) {
+      if(check(f(reflect(1), R))) return find_subtree(1, check, R, true);
+      return -1;
+    }
+    thrust(b + sz - 1);
+    int a = sz;
+    for(b += sz; a < b; a >>= 1, b >>= 1) {
+      if(b & 1) {
+        Monoid nxt = f(reflect(--b), R);
+        if(check(nxt)) return find_subtree(b, check, R, true);
+        R = nxt;
+      }
+    }
+    return -1;
+  }
+};
+
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	V a(n+1);
+	SCAN(a);
+	if(n==0){
+		if(a[0]==1) puts("1");
+		else puts("-1");
+		return 0;
+	}else if(a[0]!=0){
+		puts("-1");
+		return 0;
+	}
+	std::reverse(ALL(a));
+
+	ll usd = 0;
+	REP(i, 0, n) usd = (usd+a[i]+1)/2;
+	if(usd!=1){
+		puts("-1");
+		return 0;
+	}
+	
+	LazySegmentTree<ll,ll> tree(n+1,
+		[](ll a, ll b){return a+b;},
+		[](ll a, ll b){return a+b;},
+		[](ll a, ll b){return a+b;},
+		0, 0
+	);
+
+	tree.update(0, n, 1);
+	ll ans = n+1, pos = 0;
+	std::reverse(ALL(a));
+	a[n]--;
+	REP(i, 0, n+1) while(a[i]) {
+		if(a[i]<=tree[pos]){
+			ans += (i - pos) * a[i];
+			tree.update(pos, pos+1, -a[i]);
+			tree.update(pos+1, i, a[i]);
+			a[i] = 0;
+		}else{
+			ans += (i - pos) * tree[pos];
+			tree.update(pos+1, i, tree[pos]);
+			a[i] -= tree[pos];
+			tree.update(pos, pos+1, -tree[pos]);
+		}
+		while(tree[pos]==0) pos++;
+	}
+	printf("%ld
+", ans);
+	return 0;
+}
