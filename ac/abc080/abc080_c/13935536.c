9eaaf07517cbbc548cc9cfc0725e9d43ac510363
e1810 <v.iceele1810@gmail.com>
2020-06-02 01:35:08 +0900

    add ac/abc080/abc080_c/13935536.c

---
+++ ac/abc080/abc080_c/13935536.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define REP(i,b,e) for(int i=b; i<e; i++)
+
+int main(){
+        short n;
+        scanf("%hd", &n);
+        short f[n];
+        REP(i, 0, n){
+                f[i] = 0;
+                REP(j, 0, 10){
+                        getchar();
+                        f[i] |= (getchar()%2)<<j;
+                }
+        }
+
+        int p[n][11];
+        REP(i, 0, n) REP(j, 0, 11) scanf("%d", &p[i][j]);
+
+        int ans = -1e9;
+        REP(i, 1, 1<<10){
+                int tmp = 0;
+                REP(j, 0, n) {
+                        char c = 0;
+                        for(short a=i&f[j]; a; a>>=1) c += a&1;
+                        tmp += p[j][c];
+                }
+                if(ans<tmp) ans = tmp;
+        }
+        printf("%d
+", ans);
+        return 0;
+}
