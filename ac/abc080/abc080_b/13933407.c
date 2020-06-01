304c8b2e3766e3de84ef36a919ec3733c4c6de94
e1810 <v.iceele1810@gmail.com>
2020-06-02 00:06:39 +0900

    add ac/abc080/abc080_b/13933407.c

---
+++ ac/abc080/abc080_b/13933407.c
@@ -0,0 +1,11 @@
+main(n,fn,c){
+        n = fn = 0;
+        while((c=getchar())!='
+'){
+                n = n*10 + c-'0';
+                fn += c - '0';
+        }
+        if(n%fn) puts("No");
+        else puts("Yes");
+        return 0;
+}
