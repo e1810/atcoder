f1ace4f324bda5dc6aaa9e979c18167eb02f0bad
e1810 <v.iceele1810@gmail.com>
2020-06-02 00:35:11 +0900

    add ac/abc080/abc080_b/13934276.c

---
+++ ac/abc080/abc080_b/13934276.c
@@ -0,0 +1,10 @@
+#pragma target("avx2")
+#pragma target("Ofast")
+main(n,fn,c){
+        n = fn = 0;
+        while((c=getchar())!='
+') n=n*10+c-'0', fn+=c-'0';
+        if(n%fn) puts("No");
+        else puts("Yes");
+        return 0;
+}
