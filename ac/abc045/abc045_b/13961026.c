34ecfeb096adb2bb5a4554207317d90f479c4bed
e1810 <v.iceele1810@gmail.com>
2020-06-03 00:24:27 +0900

    add ac/abc045/abc045_b/13961026.c

---
+++ ac/abc045/abc045_b/13961026.c
@@ -0,0 +1,16 @@
+short len[3], used[3], s[3][100], d, i;
+
+void rec(t){
+        if(used[t]++>=len[t]) putchar('A'+t);
+        else rec(s[t][used[t]-1]);
+}
+
+main(){
+        for(i=0; i<3; i++){
+                while((d=getchar())!='
+'){
+                        s[i][len[i]++] = d-'a';
+                }
+        }
+        rec(0);
+}
