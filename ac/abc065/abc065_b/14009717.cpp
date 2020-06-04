02a44fdf8c00358358de370aee996f8f0fcd6cd8
e1810 <v.iceele1810@gmail.com>
2020-06-04 23:58:31 +0900

    add ac/abc065/abc065_b/14009717.cpp

---
+++ ac/abc065/abc065_b/14009717.cpp
@@ -0,0 +1,43 @@
+#pragma target("avx")
+#pragma optimize("O3")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define REP(i,b,e) for(ll i=b; i<e; i++)
+#define ALL(vec) vec.begin(),vec.end()
+#define PRINT(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define SCAN(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	V a(n);
+	SCAN(a);
+	int i = 1, ans = 0;
+	std::set<int> st;
+	st.insert(1);
+	while(true){
+		if(i==2){
+			printf("%d
+", ans);
+			return 0;
+		}
+		ans++;
+		i = a[i-1];
+		if(st.count(i)) break;
+		st.insert(i);
+	}
+	puts("-1");
+	return 0;
+}
