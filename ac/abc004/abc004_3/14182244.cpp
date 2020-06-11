6625ca664fdc4bca507b7990b104eee31961f271
e1810 <v.iceele1810@gmail.com>
2020-06-12 00:05:16 +0900

    add ac/abc004/abc004_3/14182244.cpp

---
+++ ac/abc004/abc004_3/14182244.cpp
@@ -0,0 +1,32 @@
+#pragma target("avx")
+#pragma optimize("O3")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define REP(i,b,e) for(ll i=b; i<e; i++)
+#define ALL(vec) vec.begin(),vec.end()
+#define PRINT(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define SCAN(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	int d=n/5, m=n%5;
+	int ans[6];
+	REP(i, 0, 6) ans[i] = (i+d)%6;
+	REP(i, 0, m) std::swap(ans[i], ans[i+1]);
+	REP(i, 0, 6) printf("%d", ans[i]+1);
+	puts("");
+	return 0;
+}
