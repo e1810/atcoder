86c8b2005317f5cf64f7028c658157dcb459487f
e1810 <v.iceele1810@gmail.com>
2020-06-18 23:47:55 +0900

    add ac/arc034/arc034_2/14457645.cpp

---
+++ ac/arc034/arc034_2/14457645.cpp
@@ -0,0 +1,38 @@
+#pragma target("avx")
+#pragma optimize("O3")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define REP(i,b,e) for(ll i=b; i<e; i++)
+#define ALL(vec) vec.begin(),vec.end()
+#define PRINT(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define SCAN(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	ll n;
+	scanf("%ld", &n);
+	V ans;
+	for(ll x=n; x>n-200; x--){
+		ll fx = 0, tmp = x;
+		while(tmp) fx += tmp%10, tmp /= 10;
+		if(x+fx==n) ans.push_back(x);
+	}
+	std::sort(ALL(ans));
+
+	printf("%ld
+", ans.size());
+	for(ll a: ans) printf("%ld
+", a);
+	return 0;
+}
