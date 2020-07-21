f69e6690b726cff4c7ada07f190af4e02bef7435
e1810 <e1810@s.akashi.ac.jp>
2020-07-21 17:28:21 +0900

    add ac/abc122/abc122_b/15345909.cpp

---
+++ ac/abc122/abc122_b/15345909.cpp
@@ -0,0 +1,41 @@
+#pragma target("avx")
+#pragma optimize("O3")
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+using V = std::vector<ll>;
+template<typename T> using pq = std::priority_queue<T>;
+template<typename T> using rpq = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define REP(i,b,e) for(ll i=b; i<e; i++)
+#define ALL(vec) vec.begin(),vec.end()
+#define PRINT(vec) printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");
+#define SCAN(vec) for(auto &i:vec)scanf("%ld",&i)
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+const ll INF = 1e18;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int ans = 0;
+	std::string s;
+	std::cin >> s;
+
+	for(int i=0; i<s.size(); i++){
+		for(int j=i; j<s.size(); j++){
+			for(int x=i; x<=j; x++){
+				if(s[x]!='A' and s[x]!='C' and s[x]!='G' and s[x]!='T'){
+					break;
+				}else if(x==j){
+					ans = std::max(ans, j-i+1);
+				}
+			}
+		}
+	}
+	printf("%d
+", ans);
+	return 0;
+}
