69645ab3815ba313737895b1f794bc40ff97c28a
e1810 <e1810@s.akashi.ac.jp>
2020-09-08 21:39:38 +0900

    add ac/nomura2020/nomura2020_a/16580498.cpp

---
+++ ac/nomura2020/nomura2020_a/16580498.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+int main(){
+        int h, m, k;
+        scanf("%d %d", &h, &m);
+        int s = h*60 + m;
+        scanf("%d %d", &h, &m);
+        int g = h*60 + m;
+        scanf("%d", &k);
+        printf("%d
+", std::max(0, g-s-k));
+        return 0;
+}
