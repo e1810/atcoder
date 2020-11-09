6d762ceee5bf669877aee89628a7a1d0af858bd0
e1810 <e1810@s.akashi.ac.jp>
2020-11-09 21:08:24 +0900

    add ac/cf17-final/cf17_final_b/18014861.cpp

---
+++ ac/cf17-final/cf17_final_b/18014861.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	std::string s;
+	std::cin >> s;
+	int a[3] = {};
+	for(char c: s) a[c-'a']++;
+
+	std::sort(a, a+3);
+	if(a[0]+1<a[2]) puts("NO");
+	else puts("YES");
+	return 0;
+}
