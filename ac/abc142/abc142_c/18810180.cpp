0fc98a89e77a90cc849367d67af0932441b05cf8
e1810 <e1810@s.akashi.ac.jp>
2020-12-16 21:11:11 +0900

    add ac/abc142/abc142_c/18810180.cpp

---
+++ ac/abc142/abc142_c/18810180.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	P a[n];
+	REP(i, 0, n){
+		scanf("%ld", &a[i].first);
+		a[i].second = i + 1;
+	}
+	std::sort(a, a+n);
+	REP(i, 0, n) printf("%ld
+", a[i].second);
+	return 0;
+}
