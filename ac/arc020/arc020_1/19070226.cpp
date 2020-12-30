ec270c598698eb3ae88ada1132f0484ae6e1d4eb
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 05:10:37 +0900

    add ac/arc020/arc020_1/19070226.cpp

---
+++ ac/arc020/arc020_1/19070226.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int a, b;
+	scanf("%d %d", &a, &b);
+	a = abs(a);
+	b = abs(b);
+	if(a<b) puts("Ant");
+	else if(a>b) puts("Bug");
+	else puts("Draw");
+	return 0;
+}
