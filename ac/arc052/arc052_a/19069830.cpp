e99e18a74add3dbd5f365f4b8e444ea37f31d85a
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 04:13:49 +0900

    add ac/arc052/arc052_a/19069830.cpp

---
+++ ac/arc052/arc052_a/19069830.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	std::string s;
+	std::cin >> s;
+	std::string ans;
+	for(char c: s){
+		if('0'<=c && c<='9') ans += c;
+	}
+	std::cout << ans << '
+';
+	return 0;
+}
