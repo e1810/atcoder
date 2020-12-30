77dd82cc5031b0e46d4cc4bc802187f3347c6073
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 04:56:35 +0900

    add ac/hhkb2020/hhkb2020_c/19070134.cpp

---
+++ ac/hhkb2020/hhkb2020_c/19070134.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+#define REP(i,b,e) for(ll i=b; i<e; i++)
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	bool ban[200100] = {};
+	int min = 0;
+	while(n--){
+		int a;
+		scanf("%d", &a);
+		ban[a] = true;
+		while(ban[min]) min++;
+		printf("%d
+", min);
+	}
+	return 0;
+}
