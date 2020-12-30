975ab1167c3fa1c5a35d24c00fbb434d03d204cd
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 04:18:42 +0900

    add ac/indeednow-qualb/indeednow_2015_qualb_1/19069875.cpp

---
+++ ac/indeednow-qualb/indeednow_2015_qualb_1/19069875.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	printf("%d
+", abs(a-c)+abs(b-d)+1);
+	return 0;
+}
