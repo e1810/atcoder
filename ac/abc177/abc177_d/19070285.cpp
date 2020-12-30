08e955a28b30388bfdd274eb418b8be38606d849
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 05:20:17 +0900

    add ac/abc177/abc177_d/19070285.cpp

---
+++ ac/abc177/abc177_d/19070285.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+struct UnionFind {
+	std::vector<int> data;
+
+	UnionFind(int sz) {
+		data.assign(sz, -1);
+	}
+
+	bool unite(int x, int y) {
+		x = find(x), y = find(y);
+		if(x == y) return (false);
+		if(data[x] > data[y]) std::swap(x, y);
+		data[x] += data[y];
+		data[y] = x;
+		return (true);
+	}
+
+	int find(int k) {
+		if(data[k] < 0) return (k);
+		return (data[k] = find(data[k]));
+	}
+
+	int size(int k) {
+		return (-data[find(k)]);
+	}
+};
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	UnionFind uf(n);
+	while(m--){
+		int a, b;
+		scanf("%d %d", &a, &b);
+		uf.unite(a-1, b-1);
+	}
+	int ans = 0;
+	REP(i, 0, n) ans = std::max(ans, uf.size(i));
+	printf("%d
+", ans);
+	return 0;
+}
