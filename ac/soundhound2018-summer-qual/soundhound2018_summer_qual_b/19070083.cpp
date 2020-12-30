f31a5ae95f02bc8019badf4dc47e275b4621f5ee
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 04:48:58 +0900

    add ac/soundhound2018-summer-qual/soundhound2018_summer_qual_b/19070083.cpp

---
+++ ac/soundhound2018-summer-qual/soundhound2018_summer_qual_b/19070083.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	std::string s;
+	int w;
+	std::cin >> s >> w;
+	for(int i=0; i<s.size(); i+=w){
+		putchar(s[i]);
+	}
+	putchar('
+');
+	return 0;
+}
