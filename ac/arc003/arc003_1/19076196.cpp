e0a004e3dce28134a5932dc32bde098f53127eb1
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 15:11:17 +0900

    add ac/arc003/arc003_1/19076196.cpp

---
+++ ac/arc003/arc003_1/19076196.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	getchar();
+	int sum = 0;
+	REP(i, 0, n){
+		sum += std::max(0, 'E'-getchar());
+	}
+	printf("%.10lf
+", sum / (double)n);
+	return 0;
+}
