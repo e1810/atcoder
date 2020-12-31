64c93e9465e1aca025edb1a04259cf44be0dc771
e1810 <e1810@s.akashi.ac.jp>
2020-12-31 15:44:22 +0900

    add ac/arc056/arc056_a/19076897.cpp

---
+++ ac/arc056/arc056_a/19076897.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	ll a, b, k, l;
+	scanf("%ld %ld %ld %ld", &a, &b, &k, &l);
+	printf("%ld
+", k/l*b + std::min(b, k%l*a));
+	return 0;
+}
