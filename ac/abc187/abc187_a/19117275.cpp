b0c07735ae907ed9c42bc98ba2f72471490fcf41
e1810 <e1810@s.akashi.ac.jp>
2021-01-02 21:01:03 +0900

    add ac/abc187/abc187_a/19117275.cpp

---
+++ ac/abc187/abc187_a/19117275.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int a=0, b=0;
+	while(n){
+		a += n%10;
+		n /= 10;
+	}
+	while(m){
+		b += m%10;
+		m /= 10;
+	}
+	printf("%d
+", std::max(a, b));
+	return 0;
+}
