cf6ae70ce91d3c82bad9d893cb58300705b2a69f
e1810 <e1810@s.akashi.ac.jp>
2021-01-17 21:25:59 +0900

    add ac/abc103/abc103_a/19495071.cpp

---
+++ ac/abc103/abc103_a/19495071.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int a[3];
+	scanf("%d %d %d", a, a+1, a+2);
+	std::sort(a, a+3);
+	printf("%d
+", a[2]-a[0]);
+	return 0;
+}
