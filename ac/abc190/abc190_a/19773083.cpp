062904f55f3914578a5390c57aad4f70f37a1579
e1810 <e1810@s.akashi.ac.jp>
2021-01-30 21:01:58 +0900

    add ac/abc190/abc190_a/19773083.cpp

---
+++ ac/abc190/abc190_a/19773083.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	if(a>b) puts("Takahashi");
+	if(a<b) puts("Aoki");
+	if(a==b){
+		if(c) puts("Takahashi");
+		else puts("Aoki");
+	}
+	return 0;
+}
