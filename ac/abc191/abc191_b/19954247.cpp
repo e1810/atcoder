4f0706b0739625c1c7df684f451a780bfb001008
e1810 <e1810@s.akashi.ac.jp>
2021-02-06 21:02:33 +0900

    add ac/abc191/abc191_b/19954247.cpp

---
+++ ac/abc191/abc191_b/19954247.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using ll = int_fast64_t;
+using P = std::pair<ll,ll>;
+using PP = std::pair<ll,P>;
+
+#define REP(i,b,e) for(int i=b; i<e; i++)
+#define PRINT(vec) {printf("[ ");for(auto &i:vec)printf("%ld ",i);puts("]");}
+#define fi first
+#define se second
+
+const int MOD = 1e9+7;
+int dx[] = {0, 1, 0, -1, 1, 1, -1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main(){
+	int n, x;
+	scanf("%d %d", &n, &x);
+	REP(i, 0, n){
+		int a;
+		scanf("%d", &a);
+		if(a!=x) printf("%d ", a);
+	}
+	return 0;
+}
